69590e56a1507ce2d07aa6eb54dcc93393cd4f20
Strom952 <aymeric.hessel.etu@univ-lille.fr>
2022-09-13 16:37:34 +0200

    head and eyes

--- felixthecat-master/src/fr/univlille/iut/r3_04/tp2/Felix.java
+++ felixthecat-master/src/fr/univlille/iut/r3_04/tp2/Felix.java
@@ -6,10 +6,22 @@ import javafx.scene.paint.Color;
 public class Felix {
 
 	public void drawOn(GraphicsContext gc) {
-		/*drawHead(gc);
+		drawHead(gc);
 		drawEyes(gc);
-		drawEars(gc);
+		/*drawEars(gc);
 		drawWiskers(gc);*/
 	}
+	
+	public void drawHead(GraphicsContext gc) {
+		gc.setFill(Color.GREY);
+		gc.fillOval(50, 50, 175, 200);
+	}
+	
+	public void drawEyes(GraphicsContext gc) {
+		gc.setFill(Color.BLUE);
+		gc.fillOval(90, 100, 20, 40);
+		gc.fillOval(160, 100, 20, 40);
+	}
+	
 
 }
